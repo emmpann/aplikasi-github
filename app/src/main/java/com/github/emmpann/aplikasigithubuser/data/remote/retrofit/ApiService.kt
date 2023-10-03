8a15ac35167c55945ad12ffdff02f793f5b35b40
emmpann <mhdepan@gmail.com>
2023-10-03 18:11:00 +0700

    fix api service

--- app/src/main/java/com/github/emmpann/aplikasigithubuser/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/github/emmpann/aplikasigithubuser/data/remote/retrofit/ApiService.kt
@@ -10,6 +10,8 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
+
+    @GET("search/users")
     fun getUser(
         @Query("q") id: String
     ): Call<UserResponse>
